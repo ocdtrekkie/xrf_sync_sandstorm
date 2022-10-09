37ecf98c7c502973c6ae2d35921eeb24a8ea175a
Jacob Weisz <inbox@jacobweisz.com>
2022-10-09 00:47:45 -0500

    Update system log panel

--- acp_view_log.php
+++ acp_view_log.php
@@ -1,6 +1,7 @@
 <?php
 require_once("includes/global_req_login.php");
 require_once("includes/header.php");
+require_once("includes/functions_get.php");
 
 if ($xrf_myulevel < 3)
 {
@@ -17,16 +18,16 @@ $result=mysqli_query($xrf_db, $query);
 $num=mysqli_num_rows($result);
 
 echo "<table>
-<tr><td width=50><b>ID</b></td><td width=50><b>UID</b></td><td width=225><b>Date</b></td><td width=475><b>Event</b></td></tr>";
+<tr><td width=175><b>User</b></td><td width=175><b>Date</b></td><td width=450><b>Event</b></td></tr>";
 $qq=0;
 while ($qq < $num) {
 
-$id=xrf_mysql_result($result,$qq,"id");
 $uid=xrf_mysql_result($result,$qq,"uid");
 $date=xrf_mysql_result($result,$qq,"date");
 $event=xrf_mysql_result($result,$qq,"event");
+$username=xrf_get_username($xrf_db, $uid);
 
-echo "<tr><td>$id</td><td><a href=\"acp_view_user.php?id=$uid\">$uid</a></td><td>$date</td><td>$event</td></tr>";
+echo "<tr><td><font size=2><a href=\"acp_users.php\">$username ($uid)</a></font></td><td><font size=2>$date</font></td><td><font size=2>$event</font></td></tr>";
 $qq++;
 }
 
