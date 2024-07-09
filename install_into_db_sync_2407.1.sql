61ea31bdb91c8fa56600bda74bab87d94dbc737b
Jacob Weisz <inbox@jacobweisz.com>
2024-07-09 13:21:45 -0500

    Finish release 9

--- install_into_db_sync_2407.1.sql
+++ install_into_db_sync_2407.1.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `y_nodekv` (
   `descr` varchar(32) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Node values are about',
   `nkey` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Key',
   `nvalue` varchar(128) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Value',
-  PRIMARY KEY `nodekey` (`descr`, `nkey`)
+  PRIMARY KEY (`descr`, `nkey`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Details about nodes';
